6f657935d39999631aa59cbc6eecede589591a2a
akhil_krishna <aakhilkrishna098@gmail.com>
2023-11-01 13:54:32 +0100

    init commit

---
+++ src/main/java/com/example/dummy2/Dummy2Application.java
@@ -0,0 +1,13 @@
+package com.example.dummy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dummy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dummy2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dummy2/Dummy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dummy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dummy2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
